04435a505615b8699606b6737a6362416d9f84a0
Tharun R <123483287+tharunR-17@users.noreply.github.com>
2025-06-28 11:54:28 +0530

    Create Find Subsequence of Length K With the Largest Sum.java

---
+++ Find Subsequence of Length K With the Largest Sum.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int[] maxSubsequence(int[] nums, int k) {
+        int n = nums.length;
+        int[][] pairs = new int[n][2]; 
+        
+        for (int i = 0; i < n; i++) {
+            pairs[i][0] = nums[i];
+            pairs[i][1] = i;
+        }
+
+        Arrays.sort(pairs, (a, b) -> b[0] - a[0]);
+
+        int[][] topK = Arrays.copyOf(pairs, k);
+
+        Arrays.sort(topK, (a, b) -> a[1] - b[1]);
+
+        int[] result = new int[k];
+        for (int i = 0; i < k; i++) {
+            result[i] = topK[i][0];
+        }
+
+        return result;
+    }
+}
