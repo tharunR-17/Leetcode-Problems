c40e5dffed8a0ada98d03f51fa91f6ba4d24afed
Tharun R <123483287+tharunR-17@users.noreply.github.com>
2026-02-24 18:29:53 +0530

    Create Sum of Root To Leaf Binary Numbers.java

---
+++ Sum of Root To Leaf Binary Numbers.java
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+
+    public int sumRootToLeaf(TreeNode root) {
+        return dfs(root, 0);
+    }
+
+    private int dfs(TreeNode node, int sum) {
+        if (node == null) {
+            return 0;
+        }
+
+        sum = (sum << 1) + node.val;
+
+        if (node.left == null && node.right == null) {
+            return sum;
+        }
+
+        return dfs(node.left, sum) + dfs(node.right, sum);
+    }
+}
