1744dfed41c0458fbfa71f94920671c2edfdabbe
Tharun R <123483287+tharunR-17@users.noreply.github.com>
2026-05-06 15:25:04 +0530

    Create Rotating the Box.java

---
+++ Rotating the Box.java
@@ -0,0 +1,26 @@
+class Solution {
+    public char[][] rotateTheBox(char[][] boxGrid) {
+        int row = boxGrid.length;
+        int col = boxGrid[0].length;
+        char[][] rotate = new char[col][row];
+        for (int i = 0; i < col; i++) {
+            Arrays.fill(rotate[i], '.');
+        }
+
+        for (int k = 0; k < row; k++) {
+            int bottom = col - 1;
+            for (int j = col - 1; j >= 0; j--) {
+                if (boxGrid[k][j] == '#') {
+                    rotate[bottom][row - 1 - k] = '#';
+                    bottom--;
+
+                } 
+                else if (boxGrid[k][j] == '*') {
+                    rotate[j][row - 1 - k] = '*';
+                    bottom = j - 1;
+                }
+            }
+        }
+        return rotate;
+    }
+}
